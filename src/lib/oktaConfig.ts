e79a07b6378cd2542e56218c5766002de0a91d74
Ghost119 <deeven1109@gmail.com>
2024-01-28 13:43:24 +0530

    Refactoring

--- src/lib/oktaConfig.ts
+++
@@ -1,8 +0,0 @@
-export const oktaconfig = {
-    clientId: '0oaeqppxquZ13ZPbc5d7',
-    issuer: 'https://dev-47244298.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
-    scope: ['openid', 'profile', 'email'],
-    pkce: true,
-    disableHttpsCheck: true
-}
