278101e9507912af1288529b4ec3cba8806ba260
Ghost119 <deeven1109@gmail.com>
2024-01-28 13:28:22 +0530

    Frontend files

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { HomePage } from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { oktaconfig } from './lib/oktaConfig';
+import { OktaAuth, toRelativeUrl } from '@okta/okta-auth-js';
+import { LoginCallback, Security } from '@okta/okta-react';
+import LoginWidget from './Auth/LoginWidget';
+
+const oktaAuth = new OktaAuth(oktaconfig);
+
+export const App = () => {
+  const customAuthHandler = () => {
+    history.push('/login');
+  }
+
+  const history = useHistory();
+
+  const restoreOriginalUri = async (_oktaAuth: any, originalUri: any) => {
+    history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+  };
+
+  return (
+    <div className='d-flex flex-column min-vh-100'>
+      <Security oktaAuth={oktaAuth} restoreOriginalUri={restoreOriginalUri} onAuthRequired={customAuthHandler}>
+        <Navbar />
+        <div className='flex-grow-1'>
+          <Switch>
+            <Route path='/' exact>
+              <Redirect to='/home' />
+            </Route>
+            <Route path='/home'>
+              <HomePage />
+            </Route>
+            <Route path='/search'>
+              <SearchBooksPage />
+            </Route>
+            <Route path='/checkout/:bookId'>
+              <BookCheckoutPage />
+            </Route>
+            <Route path='/login' render = {() => <LoginWidget config={oktaconfig}/>}/>
+            <Route path='/login/callback' component={LoginCallback}/>
+          </Switch>
+        </div>
+        <Footer />
+      </Security>
+    </div>
+  );
+}
+
