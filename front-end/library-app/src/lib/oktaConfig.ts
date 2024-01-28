28469e42662893c6e2bde26e5bccf0b2157f25fc
Ghost119 <deeven1109@gmail.com>
2024-01-28 14:17:07 +0530

    Final refactoring

---
+++ front-end/library-app/src/lib/oktaConfig.ts
@@ -0,0 +1,8 @@
+export const oktaconfig = {
+    clientId: '0oaeqppxquZ13ZPbc5d7',
+    issuer: 'https://dev-47244298.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scope: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+}
